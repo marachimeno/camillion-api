0dcf3cb08988623414ab7285c3ad830cf06b791d
Mara Chimeno <marachimeno@gmail.com>
2020-10-20 19:34:41 +0200

    second endpoint

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::NewsController < Api::V1::BaseController
+  before_action :set_new, only: [:show]
+
   def index
     @news = policy_scope(New)
+  end
+
+
+  def show
+  end
+
+  private
+
+  def set_new
+    @new = New.find(params[:id])
+    authorize @new  # For Pundit
   end
 end
--- app/policies/new_policy.rb
+++ app/policies/new_policy.rb
@@ -1,4 +1,8 @@
 class NewPolicy < ApplicationPolicy
+  def show?
+    true
+  end
+
   class Scope < Scope
     def resolve
       scope.all
